f2f9412e64abae3d72abd654af2c8f58626a348c
alex8Salgado <alexmanuelsalgado@gmail.com>
2025-05-07 03:33:13 -0400

    Update add_to_collection.php

--- JERSEY SHOP/add_to_collection.php
+++ JERSEY SHOP/add_to_collection.php
@@ -2,7 +2,7 @@
 session_start();
 $conn = mysqli_connect("localhost", "root", "", "jersey_shop");
 
-// Initialize session collection
+
 if (!isset($_SESSION['collection'])) {
     $_SESSION['collection'] = [];
 }
@@ -11,7 +11,7 @@ if (isset($_POST['add_to_collection']) && isset($_POST['product']) && isset($_PO
     $product = $_POST['product'];
     $image = $_POST['image'];
 
-    // Prevent duplicates in session
+    
     $alreadyAdded = false;
     foreach ($_SESSION['collection'] as $item) {
         if ($item['product'] === $product) {
@@ -26,7 +26,7 @@ if (isset($_POST['add_to_collection']) && isset($_POST['product']) && isset($_PO
             'image' => $image
         ];
 
-        // Also prevent duplicates in DB
+        
         $check = mysqli_query($conn, "SELECT * FROM collection WHERE title = '$product'");
         if (mysqli_num_rows($check) === 0) {
             $stmt = mysqli_prepare($conn, "INSERT INTO collection (title, image) VALUES (?, ?)");
